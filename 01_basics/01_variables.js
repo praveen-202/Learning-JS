33636ada995c49ec923d2c213194b93679355e2a
Praveen Kumar <120276801+praveen0000007@users.noreply.github.com>
2024-10-21 07:13:20 +0000

    Variable and Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 123456
+let accountEmail = "kumarpraveen8797@gmail.com"
+var accountPassword = "123444"
+accountCity  = "Bagalore"
+let accountState
+
+// accountId = 2 //not allowed - const datatype is constant type allowed one allocation
+
+console.log(accountId);
+
+accountEmail = "pka@gmai.com"
+accountPassword = "212"
+accountCity  = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+  
+/*
+  Prefer not to use var because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// alert("Hello  ") //we are using nodeJs, not browser
+
+console.log(3+3); console.log("Helloo"); //semicolumn is optional in Js
+
+// DataTypes-----------------------------
+
+let name = "Praveen"
+let age =18
+let isLoggedIn = false
+
+//-----Primitive datatypes-----------
+// number => 2 to power 53
+//bigint 
+//string => ""
+// bloolean => true/false
+// null=> standalone value 
+// undefined => 
+//symol =>unique
+
+//-----user define dt.----------------
+//object
+
+console.log(typeof("Praveen"));
+console.log(typeof(age));
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
